197d5d9cd7ac4ed5542d8eebd279f81eebd9fce7
JapiKredi <jasper.bongers@yahoo.com>
2024-01-12 19:45:43 +0800

    Commit

---
+++ app.py
@@ -0,0 +1,6 @@
+#filepath = "/content/drive/Food App Reviews.csv"
+
+filepath = /Users/jasper/Desktop/SemanticSearch/Food App Reviews.csvv"
+
+# Load your dataset and convert the review column to list
+data = pd.read_csv(filepath)
