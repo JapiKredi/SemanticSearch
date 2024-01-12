d768b551e7445cc58fc34d7cd8ef9fda91d19866
JapiKredi <jasper.bongers@yahoo.com>
2024-01-12 20:41:41 +0800

    Commit

--- embeddings.py
+++ embeddings.py
@@ -1,8 +1,16 @@
 import numpy as pd
 import pandas as pd
+from sentence_transformers import SentenceTransformer
+from umap import UMAP
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('Food App Reviews.csv')
 
 reviews = df['Review'].tolist()
 
+model_name = "all-MiniLM-L6-v2"
+model = SentenceTransformer(model_name)
+
+# Generate sentence embeddings
+embeddings = model.encode(reviews)
 
