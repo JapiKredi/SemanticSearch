44cf76ee4140a25ea9966f680f041aadaad05654
JapiKredi <jasper.bongers@yahoo.com>
2024-01-12 21:46:55 +0800

    Commit

--- embeddings.py
+++ embeddings.py
@@ -3,6 +3,9 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer
 from umap import UMAP
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+import plotly.express as px
 
 df = pd.read_csv('Food App Reviews.csv')
 
@@ -13,4 +16,94 @@ model = SentenceTransformer(model_name)
 
 # Generate sentence embeddings
 embeddings = model.encode(reviews)
+
+# Generate tensor embeddings
+embeddings = model.encode(reviews, convert_to_tensor=True)
+
+# Reduce dimensionality with UMAP
+reducer = UMAP(n_components = 2, metric='cosine')
+embeddings_2d = reducer.fit_transform(embeddings)
+
+# Plot the embeddings
+plt.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1], marker = 'o')
+plt.title('Food App Review Embeddings')
+plt.show()
+
+# Apply K-means clustering
+num_clusters = 3
+kmeans = KMeans(n_clusters=num_clusters)
+cluster_labels = kmeans.fit_predict(embeddings_2d)
+
+# Add cluster labels as a new column to the DataFrame
+df['cluster_label'] = cluster_labels
+
+# Create a DataFrame for the data
+cluster_df = pd.DataFrame({
+    'x': embeddings_2d[:, 0],
+    'y': embeddings_2d[:, 1],
+    'label': cluster_labels,
+    'sentence': df['Review']
+})
+
+# Create an interactive scatter plot using plotly
+fig = px.scatter(
+    cluster_df,
+    x='x', y='y',
+    color='label',
+    hover_name='sentence',
+    title='Food App Reviews 2D Embeddings',
+    labels={'label': 'Cluster'},
+    width=800,  # Adjust the width as desired
+    height=600,  # Adjust the height as desired
+)
+
+fig.update_traces(
+    marker=dict(size=8)  # Adjust the size value as needed
+)
+
+# Set the background color to black
+fig.update_layout(
+    plot_bgcolor='white',
+)
+
+fig.show()
+
+# Reduce dimensionality to 3 dimensions with UMAP
+reducer = UMAP(n_components=3, metric='cosine')
+embeddings_3d = reducer.fit_transform(embeddings)
+
+# Apply K-means clustering
+num_clusters = 3
+kmeans = KMeans(n_clusters=num_clusters)
+cluster_labels = kmeans.fit_predict(embeddings)
+
+# Add cluster labels as a new column to the DataFrame
+df['cluster_label'] = cluster_labels
+
+
+# Create a DataFrame for the data
+cluster_df = pd.DataFrame({
+    'x': embeddings_3d[:, 0],
+    'y': embeddings_3d[:, 1],
+    'z': embeddings_3d[:, 2],
+    'label': cluster_labels,
+    'sentence': df['Review']
+})
+
+# Create a 3D scatter plot using plotly
+fig = px.scatter_3d(
+    cluster_df,
+    x='x', y='y', z='z',
+    color='label',
+    hover_name='sentence',
+    title='Food App Reviews 3D Embeddings',
+    labels={'label': 'Cluster'},
+)
+
+fig.update_traces(
+    marker=dict(size=5)  # Adjust the size value as needed
+)
+
+fig.show()
+
 
